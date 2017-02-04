ecbb487fef65661bcf3245645202681bfa7ca7ff
djawny <djawny@op.pl>
2017-02-04 16:00:47 +0100

    zmiana 04/02

--- app/src/main/java/com/sdaacademy/jawny/daniel/mediaplayer/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/mediaplayer/MainActivity.java
@@ -4,17 +4,21 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private MediaPlayer dubstep;
+    private SeekBar volumeBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dubstep = MediaPlayer.create(this, R.raw.bensound_dubstep);
+        volumeBar = (SeekBar) findViewById(R.id.volumeBar);
+        dubstep = MediaPlayer.create(getApplicationContext(), R.raw.bensound_dubstep);
+        volumeBar.setOnSeekBarChangeListener(this);
     }
 
     public void buttonClicked(View view) {
@@ -25,8 +29,25 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.pauseButton:
                 if (dubstep.isPlaying())
-                    dubstep.stop();
+                    dubstep.pause();
+                else dubstep.start();
                 break;
         }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        float volume = progress / 100f;
+        dubstep.setVolume(volume, volume);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
